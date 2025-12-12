b2c739f2efbd1f5919736117b99b05f476c0d378
mariebarke <maba2811@student.hnu.de>
2025-12-12 15:44:40 +0100

    UI CoffeSHop form

--- src/main/java/Projektarbeit/CoffeShop.java
+++ src/main/java/Projektarbeit/CoffeShop.java
@@ -1,4 +1,21 @@
 package Projektarbeit;
 
+import javax.swing.*;
+
 public class CoffeShop {
+    private JPanel CoffePanel;
+    private JLabel titleLabel;
+    private JLabel createDrinkLabel;
+    private JLabel drinkLabel;
+    private JComboBox drinkcomboBox;
+    private JLabel groeßeLabel;
+    private JComboBox groeßecomboBox;
+    private JLabel flavourLabel;
+    private JRadioButton vanilleRadioButton;
+    private JRadioButton caramellRadioButton;
+    private JRadioButton pistazieRadioButton;
+    private JLabel milchLabel;
+    private JComboBox comboBox1;
+    private JButton createButton;
+    private JTextArea ausgabeTextArea;
 }
